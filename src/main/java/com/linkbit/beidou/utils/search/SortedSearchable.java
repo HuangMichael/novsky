431098d2f255dab0c6ff5cd2bda4263b9f7e80a9
huangbin <876301469@qq.com>
2016-11-22 16:29:38 +0800

    c

---
+++ src/main/java/com/linkbit/beidou/utils/search/SortedSearchable.java
@@ -0,0 +1,51 @@
+package com.linkbit.beidou.utils.search;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * Created by huangbin on 2016/11/18.
+ * <p>
+ * 复合条件查询接口
+ */
+public interface Searchable {
+
+
+    /**
+     * @param searchPhrase 搜索关键字组合
+     * @param paramSize    搜索条件个数
+     * @param pageable     可分页
+     * @return
+     */
+    Page findByConditions(String searchPhrase, int paramSize, Pageable pageable);
+
+
+    /**
+     * @param searchPhrase 搜索关键字组合
+     * @param paramSize    搜索条件个数
+     * @return
+     */
+    List findByConditions(String searchPhrase, int paramSize);
+
+
+    /**
+     * @param searchPhrase 搜索关键字组合
+     * @param paramSize    搜索条件个数
+     * @param sort         排序
+     * @param pageable     可分页
+     * @return
+     */
+    Page findByConditions(String searchPhrase, int paramSize, String[] sort, Pageable pageable);
+
+
+    /**
+     * @param searchPhrase 搜索关键字组合
+     * @param paramSize    搜索条件个数
+     * @param sort         排序
+     * @return
+     */
+    List findByConditions(String searchPhrase, int paramSize, String[] sort);
+
+}
