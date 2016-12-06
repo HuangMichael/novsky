36368e8f28f06ca5b45f3088f1c7b0c4a10097af
huangbin <876301469@qq.com>
2016-12-06 11:26:19 +0800

    c

--- src/main/java/com/subway/Application.java
+++ src/main/java/com/subway/Application.java
@@ -7,6 +7,7 @@ import com.subway.liseners.AppilicationStopped;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,14 +15,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 /**
- *
+ * 初始化入口
  */
 @EnableTransactionManagement //启用事务管理
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
-@EnableScheduling
+@EnableScheduling //开启调度
+/*@EnableCaching //开启缓存*/
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/subway/dao/locations/LocationsRepository.java
+++ src/main/java/com/subway/dao/locations/LocationsRepository.java
@@ -1,6 +1,9 @@
 package com.subway.dao.locations;
 
 import com.subway.domain.locations.Locations;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +14,13 @@ import java.util.List;
  * Created by huangbin on 2016/3/15 0008.
  * 位置信息查询接口
  */
+
+@CacheConfig(cacheNames = "locationsList")
 public interface LocationsRepository extends CrudRepository<Locations, Long> {
     /**
      * 查询所有设备类别
      */
+    @Cacheable(key = "#p0")
     @Query("SELECT l FROM Locations l")
     List<Locations> findAll();
 
@@ -41,6 +47,8 @@ public interface LocationsRepository extends CrudRepository<Locations, Long> {
      * @param locations
      * @return 保存位置信息
      */
+
+    @CachePut(key = "#p0.id")
     Locations save(Locations locations);
 
     /**
@@ -51,13 +59,18 @@ public interface LocationsRepository extends CrudRepository<Locations, Long> {
 
     List<Locations> findByParent(Long id);
 
-    /**
-     * 根据位置编码模糊查询
-     */
-    List<Locations> findByLocationLike(String location);
 
+    /**
+     * @param location
+     * @return
+     */
     List<Locations> findByLocationStartingWith(String location);
 
+    /**
+     * @param location
+     * @param status
+     * @return
+     */
     List<Locations> findByLocationStartingWithAndStatus(String location, String status);
 
     /**
